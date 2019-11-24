b447704c53c4e0caaa654b07e6d51c493d7acb6b
Adriana Venegas <adrivegas08@gmail.com>
2019-11-24 14:52:14 -0500

    Implementación de Lazy Loading

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BannerComponent } from './componentes/banner/banner.component';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './componentes/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [BannerComponent, HomeComponent],
-  imports: [HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule],
   providers: [],
   bootstrap: []
 })
